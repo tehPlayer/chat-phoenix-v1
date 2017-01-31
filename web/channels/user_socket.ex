8782e7d5917e379d483027fb703000db1a595a5c
Marcin Jan Adamczyk <adamczyk.marcin@outlook.com>
2017-01-31 15:14:35 +0100

    production config

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,8 @@ defmodule ChatPhoenix.UserSocket do
   channel "room:lobby", ChatPhoenix.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
@@ -35,4 +36,6 @@ defmodule ChatPhoenix.UserSocket do
   #
   # Returning `nil` makes this socket anonymous.
   def id(_socket), do: nil
+
+
 end
